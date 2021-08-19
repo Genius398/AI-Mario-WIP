6c425af43d50d7b16b491e00299e9ca807ae3b0e
Anantjit <anantjitchander@gmail.com>
2021-08-19 19:36:42 +0530

    Add files via upload
    Add 1000000+ files which took 100000 centauries XD

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
