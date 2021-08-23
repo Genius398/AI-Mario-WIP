057c56a88bd262242cc933ce88b7e24a97c1115c
Anantjit <anantjitchander@gmail.com>
2021-08-23 18:30:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+img ="";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
+	img = loadImage("mario05.png");
+	
 }
 
 function setup() {
-	canvas = createCanvas(1240,336);
+	canvas = createCanvas(650,400);
+	video = createCapture(VIDEO);
+	video.size(600,300)
+	poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose', gotPoses);
 	canvas.parent('canvas');
 
 	instializeInSetup(mario);
@@ -13,9 +25,33 @@ function setup() {
 
 function draw() {
 	game()
+	background("#D3D3D3");
+	if(noseX <300){
+		marioX = marioX - 1;
+	}
+
+	if(noseX > 300){
+		marioX = marioX + 1;
+	}
+
+	if(noseY <150){
+		marioY = marioY-1;
+	}
+
+	image(img, marioX, marioY, 40,70);
 }
 
+function modelLoaded(){
+	console.log("Model Loaded");
+}
 
+function gotPoses(results){
+	if (results.length > 0){
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("y=" + noseY  +", x=" + noseY);
+	}
+}
 
 
 
